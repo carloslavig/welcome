c5dab40c3cd8e985c034e09358e9a873cc6f7036
carloslavig <carloslavig@gmail.com>
2024-11-03 17:25:03 -0300

    funcionou

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,21 @@
 import java.util.Scanner;
+import java.time.LocalDate;
+
 public class Main
 {
 
     public static void main(String[] args)
     {
-        Scanner scanner = new Scanner(System.in);
         //Olá {nome}. Hoje é {dia-da-semana}, Bom dia.
+
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("informe seu nome");
         String nome = scanner.nextLine();
+
+        LocalDate hoje =LocalDate.now();
+        System.out.println(hoje.getDayOfWeek());
+
 
     }
 }
